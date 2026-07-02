4859355bc13d90809ef4418f53cf7abf0144f9a9
Lalit Kakkar <lalitkakkar50@gmail.com>
2026-07-02 21:21:32 +0530

    fix(build): resolve Edge Runtime incompatibility in blog OG image

--- app/blog/[slug]/opengraph-image.tsx
+++ app/blog/[slug]/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from '@vercel/og'
 import { siteConfig } from '@/config/site'
 import { getBlogPost } from '@/lib/mdx'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 export const size = { width: 1200, height: 630 }
 export const contentType = 'image/png'
 
